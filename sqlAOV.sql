1312bb002fe20d1544cf75fcb635c66497dfef03
CalInca <52050196+CalInca@users.noreply.github.com>
2019-06-21 23:19:53 -0700

    Delete sqlAOV.sql

--- sqlAOV.sql
+++
@@ -1,7 +0,0 @@
-SELECT order_number as Cust_Order,
-       strftime('%W', shipment_date) AS Week,
-       COUNT(*) as Cust_Count,
-       ROUND(AVG(cast(substr(order_value,2) as decimal)),2) as Avg_Spend
-  FROM orders
-GROUP BY order_number, strftime('%W', shipment_date)
-ORDER BY strftime('%W', shipment_date);
